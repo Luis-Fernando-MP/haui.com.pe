07b433273d888085f304eb62b734ffddbdca5f1e
Haui <luigfmp@gmail.com>
2025-09-25 21:33:31 -0500

    refactor: 🛠️ Update Button component and utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function cn(...classes: (string | undefined | false)[]) {
   return classes.filter(Boolean).join(' ')
 }
+
+export const cl = (active: boolean, className: string = 'active', or: string = '') => {
+  return active ? className : or
+}
